ed0a1711ee3ecc4058447034e77b01cd544bb5d5
Tin Le <tinle1596@gmail.com>
2019-05-08 22:09:17 -0700

    Formatting bootstrap vue card

--- src/components/attendance/Students.vue
+++ src/components/attendance/Students.vue
@@ -1,69 +1,52 @@
 <template>
-    <div id="students">                
-        <div v-for="student in filteredSearchStudents" :key="student.id">
-            <b-card bg-variant="dark" text-variant="light" style="max-width: 90%; max-height: 10%;">
-                <b-col>
-                    
-                </b-col>
-            </b-card>          
-        </div>        
-    </div>
+  <div id="students">    
+    <b-card-group v-for="student in filteredSearchStudents" :key="student.id">
+        <b-card
+          :header="student.firstName + ' ' + student.lastName"          
+          header-text-variant="black" 
+          bg-variant="info"
+          style="max-width:25rem; max-height: 15rem"
+          class="overflow-hidden">
+          <b-col md="6">
+            <b-card-img 
+            src="https://i.imgur.com/y7oYbiP.jpg"             
+            style="max-width:5rem; max-height: 5rem"
+            class="rounded-0"
+            >
+            </b-card-img>
+          </b-col>                      
+          <b-col md="6">
+            <b-card-text>
+              test
+            </b-card-text>
+          </b-col>            
+        </b-card>
+    </b-card-group>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import attendanceButton from './common/AttendanceButton'
+import { mapGetters } from "vuex";
+//import attendanceButton from './common/AttendanceButton'
 
-    export default {
-        name: 'Students',
-        props:{
-        },
-        components:{
-            attendanceButton
-        },
-        data() {
-            return {
-                
-            }
-        },
-        computed:{
-            ...mapGetters([
-                'filteredSearchStudents'
-            ]),        
-        },             
-    }
+export default {
+  name: "Students",
+  props: {},
+  components: {
+    //attendanceButton
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["filteredSearchStudents"])
+  }
+};
 </script>
 
 <style scoped>
-img{
-    max-width: 100%;
-    min-height: 25vh
+label {
+  display: block;
+  line-height: 1.5em;
 }
-.student-image{
-    width: 25vh;
-    min-height: 20vh;
-    max-height: auto;
-    margin: 5px;
-    padding: 10px
-}
-.student-card{
-    background: #45A29E;
-    width: 25pc;
-}
-
-.attendance-button{
-    background: blue;
-    float: right;
-    border: 20px white;
-    padding: 5px;
-    height: 20px;
-    width: 150px;
-}
-
-label{
-    display: block;
-    line-height: 1.5em;
-}
-
-
 </style>
