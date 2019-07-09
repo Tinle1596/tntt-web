9ef19c2bc1717076b1600d414c0ac715f9d36d4e
Tin Le <tinle1596@gmail.com>
2019-07-08 21:03:30 -0700

    install vuetify

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import store from './store/index'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import Vuetify from 'vuetify'
 
 Vue.use(BootstrapVue);
+Vue.use(Vuetify);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
