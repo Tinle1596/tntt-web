daa79ba0d8ff1c3ca04dbfa1d799808a03c9ebdc
Tin Le <tinle1596@gmail.com>
2019-10-13 16:02:27 -0700

    Initial work on crud data table

--- src/components/crud/students/StudentCrud.vue
+++ src/components/crud/students/StudentCrud.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-        {{getStudents}}
+         <v-data-table
+         :headers="headers"
+         :items="getStudents"
+         :items-per-page="10"
+         >
+         <template v-slot:top>
+             <v-toolbar flat color="white">
+                 <v-toolbar-title>Mange Students</v-toolbar-title>
+                 <v-divider
+                 class="mx-4"
+                 inset
+                 vertical
+                 ></v-divider>
+                 <v-spacer></v-spacer>
+                 <v-dialog></v-dialog>
+             </v-toolbar>
+         </template>
+         </v-data-table>
     </div>
 </template>
 
@@ -10,7 +27,38 @@ import {mapGetters} from 'vuex'
     export default {
         data() {
             return {
-                
+                headers: [
+                    {
+                        text: 'First Name',
+                        align: 'left',
+                        sortable: true,
+                        value: 'firstName'
+                    },
+                    {
+                        text: 'Last Name',
+                        align: 'left',
+                        sortable: true,
+                        value: 'lastName'
+                    },
+                    {
+                        text: 'Nganh',
+                        align: 'left',
+                        sortable: true,
+                        value: 'group'
+                    },
+                    {
+                        text: 'Sex',
+                        align: 'left',
+                        sortable: false,
+                        value: 'sex'
+                    },
+                    {
+                        text: 'Phone Number',
+                        align: 'left',
+                        sortable: false,
+                        value: 'phoneNumber'
+                    }
+                ],
             }
         },
         components:{
